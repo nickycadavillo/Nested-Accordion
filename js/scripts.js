57ec3631265d08439ad19f8ecb108c3af7f93584
Nicky Cadavillo <nicoleacadavillo@gmail.com>
2016-02-10 18:18:12 -0500

    giving up

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,32 @@
 // trying to open first row
 
-function openAcc() {
+// function openAcc() {
+// var heading = document.getElementsByClassName('heading-big')
+//
+// var paragraph = document.getElementsByClassName('default')
+  // if (paragraph.hasAttribute('default')) {
+  //    paragraph.removeAttribute('default');
+//   }
+// };
 
-}
-var open = document.getElementById('row-one')
-  open.onclick = openAcc;
+var heading = document.querySelector('h2');
+var paragraph = document.getElementsByClassName('default');
+
+heading.addEventListener('click', function() {
+  alert("Hi");
+  // document.getElementsByClassName('default').className = "";
+  // if (paragraph.className === "default"){
+  //     paragraph.className"show";
+  //   }
+  //   else {
+  //     paragraph.className === "default"
+  //     }
+  // if (paragraph.classList.contains(default)){
+  //   paragraph.classList.remove(default);
+  //   paragraph.classList.add(show);
+  // if (paragraph.classList === "default"){
+  //   paragraph.classList === "show";
+  // }
+  // else {
+  //   paragraph.classList === "default"
+});
