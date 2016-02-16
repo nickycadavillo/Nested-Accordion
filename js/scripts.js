9fc95e860167e149073cd576bb8068aa56ff9cf0
Nicky Cadavillo <nicoleacadavillo@gmail.com>
2016-02-16 13:11:52 -0500

    updating scripts and setting up jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,26 @@
-// trying to open first row
 
-// function openAcc() {
-// var heading = document.getElementsByClassName('heading-big')
-//
-// var paragraph = document.getElementsByClassName('default')
-  // if (paragraph.hasAttribute('default')) {
-  //    paragraph.removeAttribute('default');
-//   }
-// };
+$(function () {
+  'use strict';
 
-var heading = document.querySelector('h2');
-var paragraph = document.getElementsByClassName('default');
 
-heading.addEventListener('click', function() {
-  alert("Hi");
-  // document.getElementsByClassName('default').className = "";
-  // if (paragraph.className === "default"){
-  //     paragraph.className"show";
-  //   }
-  //   else {
-  //     paragraph.className === "default"
-  //     }
-  // if (paragraph.classList.contains(default)){
-  //   paragraph.classList.remove(default);
-  //   paragraph.classList.add(show);
-  // if (paragraph.classList === "default"){
-  //   paragraph.classList === "show";
-  // }
-  // else {
-  //   paragraph.classList === "default"
+
+
+
+
+
 });
+
+
+
+
+
+
+// START JavaScript EXAMPLE //
+
+// var heading = document.querySelector('h2');
+// var paragraph = document.getElementsByClassName('default');
+//
+// heading.addEventListener('click', function() {
+//   alert("Hi");
+
+// END JavaScript EXAMPLE //
