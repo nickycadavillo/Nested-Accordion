ce30a44db60528fdb3f5a651d7256b3e7b8d31a9
Nicky Cadavillo <nicoleacadavillo@gmail.com>
2016-02-16 22:46:23 -0500

    big accordion opens along with sub accordion, at least individually though

--- js/scripts.js
+++ js/scripts.js
@@ -4,17 +4,14 @@ $(function () {
 $('h2').click(function(){
   // alert("clicked"); // checking if targeting right element
   // $('.default').siblings().next().toggle('.show-p'); // opens ALL big accordions
-  // $('.default').toggle('.show-p');
-  // $('.default').not().hide();
-  $(this).siblings().next().toggle('.show-p');
+  $(this).siblings().toggle('h3'); //opens accordion
 
 });
 
 
 $('h3').click(function(){
-  // alert("clicked");
-  // $('.default').toggle('.show-p');
-  $(this).siblings().next().toggle('.show-p');
+  $(this).next().toggle(); //since sub accordion is already open, due to function above, it closes it
+  // how to have the p element closed by default so that i can toggle it? that is the question!
 });
 
 
